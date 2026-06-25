3115219c666ecc1b1d0a413caf1bd65a461aa05c
IJakutow <igor.jakutow@cdtnue.onmicrosoft.com>
2026-06-25 09:55:49 +0200

    Tabelle aus Tabelle

--- project_1/tabelle_aus_tabelle.sql
+++ project_1/tabelle_aus_tabelle.sql
@@ -1,3 +1,5 @@
+--Wiederholung:
+
 select * from mein_test3;
 
 sp_rename 'mein_test3', 'kermit'
@@ -31,6 +33,54 @@ drop table mein_test1
 sp_rename 'mein_test_9', 'vornamennn'
 
 drop table mein_test4
+
+--Tabelle aus Tabelle selektiv
+
+select * from kermit;
+
+select column_name, data_type, character_maximum_length 
+from INFORMATION_SCHEMA.COLUMNS 
+where TABLE_NAME = 'kermit';
+
+create table kermit_archive (
+nummer int,
+vname varchar(10)
+);
+
+insert into kermit_archive
+select * from kermit;
+
+select * from kermit_archive;
+delete from kermit_archive;
+
+insert into kermit_archive
+select * from kermit
+where vorname <> 'Kermit' and vorname != 'null' 
+and vorname is not null
+
+create table kermit_archive_2 (
+id int identity(1,1),
+vname varchar(10)
+);
+
+select * from kermit_archive_2
+
+insert into kermit_archive_2
+select vorname from kermit;
+
+select * from kermit_archive_2;
+
+select * into kermit_archive_3 from kermit;
+select * from kermit_archive_3;
+
+select * into kermit_archive_2 from kermit;
+
+select * into neu_kermit from kermit_archive_2;
+select * from neu_kermit;
+
+insert into neu_kermit values ('asök');
+
+
 
 
 
