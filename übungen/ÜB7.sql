c461f5aacde90e33bd724f174ed31d5e4bb5df42
IJakutow <igor.jakutow@cdtnue.onmicrosoft.com>
2026-07-03 10:28:24 +0200

    ÜB7 Ende

--- übungen/ÜB7.sql
+++ übungen/ÜB7.sql
@@ -50,7 +50,6 @@ where mwi.intnr = i.intnr
 order by vorname
 
 --5.Welcher meiner Freunde liest gerne?
-
 select vorname, nachname
 from tl3_manwoman mw
 join tl3_mw_interessen mwi on mwi.mwnr = mw.mwnr
@@ -80,9 +79,20 @@ from tl3_manwoman
 where nachname = 'Kron'
 )
 
+--Prüfung:
+select * from tl3_mw_interessen
+where mwnr = 10;
+
+select mw.nachname, mw.vorname, count(*)
+from tl3_interessen i
+join tl3_mw_interessen mwi on mwi.intnr=i.intnr
+join tl3_manwoman mw  on mw.mwnr=mwi.mwnr 
+where mw.nachname = 'Kron'
+group by mw.nachname, mw.vorname
+
 --7. Welcher meiner Freunde (nachname, vorname) hat die gleichen Hobbies wie Lecter?
 
-select vorname, nachname, i.inttext
+select distinct vorname, nachname
 from tl3_manwoman mw
 join tl3_mw_interessen mwi on mwi.mwnr = mw.mwnr
 join tl3_interessen i on i.intnr = mwi.intnr --Nur wenn wir Interesse auch zeigen möchten!
@@ -110,6 +120,31 @@ where nachname='lecter'
 )
 )
 
+-- Aufgabe 7
+SELECT nachname, vorname
+FROM tl3_manwoman
+WHERE mwnr <> (
+    SELECT mwnr
+    FROM tl3_manwoman
+    WHERE nachname = 'Lecter'
+)
+AND mwnr IN
+(
+    SELECT mwnr
+    FROM tl3_mw_interessen
+    WHERE intnr IN
+    (
+        SELECT intnr
+        FROM tl3_mw_interessen
+        WHERE mwnr =
+        (
+            SELECT mwnr
+            FROM tl3_manwoman
+            WHERE nachname = 'Lecter'
+        )
+    )
+);
+
 --8. Welche intnr wurden am häufigsten genannt und wie oft? Sternchenaufgabe
 -- Tipp: count und max aber ohne top
 select mwi.intnr, count(mwi.intnr)
@@ -119,11 +154,23 @@ having count(mwi.intnr) =
 (
 select max(inter.Anzahl)
 from
-(select mwi.intnr Nummer, count(*) Anzahl
+(select  count(*) Anzahl
 from tl3_mw_interessen mwi 
 group by mwi.intnr) inter 
 )
 
+SELECT intnr, COUNT(*) AS anzahl
+FROM tl3_mw_interessen
+GROUP BY intnr
+HAVING COUNT(*) = (
+SELECT MAX(anzahl_berechnet)
+FROM (
+SELECT COUNT(*) AS anzahl_berechnet
+FROM tl3_mw_interessen
+GROUP BY intnr
+) AS Nebentabelle
+);
+ 
 
 --Prüfung:
 select intnr, count(*)
@@ -149,7 +196,7 @@ group by mwi.intnr) inter
 
 --10.Wer hat genauso viele Interessen wir die Person mit dem Nachnamen Tuck?
 
-select distinct mw.vorname, mw.nachname
+select mw.vorname, mw.nachname
 from tl3_manwoman mw
 where mw.mwnr in 
 	(
@@ -166,3 +213,32 @@ where mw.mwnr in
 			where mw.nachname = 'Tuck')
 		)
 	)
+
+-- Subselect mit Join
+SELECT m.nachname, m.vorname, COUNT(*) AS anzahl_interessen
+FROM tl3_manwoman m
+JOIN tl3_mw_interessen mwi ON m.mwnr = mwi.mwnr
+GROUP BY m.mwnr, m.nachname, m.vorname
+HAVING COUNT(*) = (
+SELECT COUNT(*) 
+FROM tl3_manwoman t_m
+JOIN tl3_mw_interessen t_mwi ON t_m.mwnr = t_mwi.mwnr
+WHERE t_m.nachname = 'Tuck'
+)
+AND m.nachname <> 'Tuck';
+
+
+--11. Ein oder mehrere Hobbies wurden am häufigsten genannt.
+--Was ist es und von wem wurde es genannt?
+
+select mw.mwnr, mw.nachname, mw.vorname, i.inttext 
+from tl3_interessen i 
+join tl3_mw_interessen mwi on i.intnr=mwi.intnr 
+join tl3_manwoman mw on mwi.mwnr=mw.mwnr 
+where mwi.intnr in 
+( 
+select top 1 with ties intnr 
+from tl3_mw_interessen 
+group by intnr 
+order by count(*) desc 
+);
